c225683779c342005ca1f80e94618853ccf53ae8
juliapaesv <juliapaesv@gmail.com>
2024-12-11 22:10:44 -0300

    Testes feitos

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,50 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Insere múltiplas tarefas', () => {
+    cy.visit('');
+  
+    const tasks = [
+      'Tarefa 1',
+      'Tarefa 2',
+      'Tarefa 3'
+    ];
+  
+    tasks.forEach(task => {
+      cy.get('[data-cy=todo-input]')
+        .type(`${task}{enter}`);
+    });
+  
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', tasks.length)
+      .each((item, index) => {
+        cy.wrap(item).should('have.text', tasks[index]);
+      });
+  });
+  
+  it('Marca tarefa como completa', () => {
+    cy.visit('');
+  
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa para completar{enter}');
+  
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click();
+  
+    cy.get('[data-cy=todos-list] > li')
+      .should('have.class', 'completed');
+  });
+  
+  it('Verifica input vazio não adiciona tarefa', () => {
+    cy.visit('');
+  
+    cy.get('[data-cy=todo-input]')
+      .type('{enter}');
+  
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
 });
